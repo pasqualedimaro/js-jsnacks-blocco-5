544c8aeb668542454bb1c1abb6a6c5309bf5dda1
pasqualedimaro <pakydm15@gmail.com>
2025-06-15 15:51:45 +0200

    use map and stamp

--- snack-2/script.js
+++ snack-2/script.js
@@ -5,4 +5,8 @@ const people = [
 ];
 
 // Stampa in console tutti i nomi
+// uso map per estrarre i nomi e poi stamparli
+const names = people.map(person => person.name);
+console.log(`'${names[0]}, ${names[1]}, ${names[2]}'`);
+
 // Risultato: 'Paolo', 'Giulia', 'Marco'

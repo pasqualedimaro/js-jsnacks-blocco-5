1b0f7bf2afaed285ee4b6cee5e787444f38cb67e
pasqualedimaro <pakydm15@gmail.com>
2025-06-15 16:10:57 +0200

    use find to locate student witch id 2

--- snack-7/script.js
+++ snack-7/script.js
@@ -20,4 +20,6 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+const student = students.find(student => student.id === 2);
+console.log(student);
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
